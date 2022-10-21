aca542604251317e81510bc7e83a777c1c8fba82
Vitalii Khmelko <vitalii.khmelko@gmail.com>
2022-10-21 11:29:37 -0400

    Trip calculator functionality

--- src/app/services/trip-calculator.service.spec.ts
+++ src/app/services/trip-calculator.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { Attendant } from '../models/attender';
 
 import { TripCalculatorService } from './trip-calculator.service';
 
@@ -12,5 +13,42 @@ describe('TripCalculatorService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('calculateStudentExpenses', () => {
+    it('should return 0 if expenses are undefined', () => {
+      const result = service.calculateStudentExpenses([undefined as any]);
+
+      expect(result).toBe(0);
+    })
+  });
+
+  describe('calculateBelongings', () => {
+    it('should return total 0 if no attendants', (): void => {
+      const result = service.calculateBelongings([])
+      expect(result.total).toBe(0);
+      expect(result.belongings).toEqual([])
+    });
+
+    it('should make calculations', (): void => {
+      const attendants: Attendant[] = [{
+        name: 'John',
+        expenses: [{ cost: 2.56 }, { cost: 0.01 }, { cost: 0.02 }]
+      }, {
+        name: 'Sarah',
+        expenses: [{ cost: 6.99 }, { cost: 5.98 }, { cost: 7.59 }]
+      }, {
+        name: 'Ivan',
+        expenses: [{ cost: 13.01 }, { cost: 4.07 }, { cost: 3.09 }]
+      }]
+
+      const result = service.calculateBelongings(attendants);
+
+      expect(result.total).toBe(43.32);
+      expect(result.belongings).toEqual([
+        { who: 'John', whom: 'Sarah', amount: 6.12 },
+        { who: 'John', whom: 'Ivan', amount: 5.73 }
+      ]);
+    })
   });
 });
--- src/app/services/trip-calculator.service.ts
+++ src/app/services/trip-calculator.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Attendant } from '../models/attender';
 import { Expenses } from '../models/expenses';
 import { UserBelongings } from '../models/user-belongings';
+import { round } from '../shared/calculations.helper';
 
 /**
  * Calculates each user expenses for the trip
@@ -18,27 +19,101 @@ export class TripCalculatorService {
    * @param attendants Trip attendants
    */
   calculateBelongings(attendants: Attendant[]): { total: number, belongings: UserBelongings[] } {
-    const total: number = attendants.reduce((cost: number, attendant: Attendant) => {
-      return cost += this.calculateUserExpenses(attendant.expenses);
-    }, 0)
+    const actualAttendantsSpends = this.getActualAttendantsSpends(attendants);
+
+    const totalSpends = round(actualAttendantsSpends.reduce((value: number, { amount }) => {
+      return value += amount;
+    }, 0));
+
+
+    const belongings: UserBelongings[] = this.calculateStudentsBelongings(actualAttendantsSpends, totalSpends);
 
     return {
-      total,
-      belongings: [{
-        who: attendants[0].name,
-        whom: attendants[1].name,
-        amount: 30,
-      }]
+      total: totalSpends,
+      belongings,
     };
+
   }
 
   /**
    * Calculate user expenses for the current trip
    * @param expenses 
    */
-  calculateUserExpenses(expenses: Expenses[]): number {
-    return expenses.reduce((aggregated: number, element: Expenses) => {
+  calculateStudentExpenses(expenses: Expenses[]): number {
+    const userExpenses = expenses.reduce((aggregated: number, element: Expenses) => {
       return aggregated + element?.cost || 0
     }, 0);
+
+    return round(userExpenses);
   }
+
+  /**
+   * Get amount of money that has been spent by each user during the trip
+   * 
+   * @param attendants Trip attendants
+   */
+  private getActualAttendantsSpends(attendants: Attendant[]): { name: string, amount: number }[] {
+    const actualSpendsForEachAttendant: { name: string, amount: number }[] = [];
+
+    if (attendants.length > 0) {
+      actualSpendsForEachAttendant.push({
+        name: attendants[0].name,
+        amount: this.calculateStudentExpenses(attendants[0].expenses),
+      });
+
+      for (let index = 1; index < attendants.length; index++) {
+        const attendant: Attendant = attendants[index];
+
+        const amount = this.calculateStudentExpenses(attendant.expenses);
+
+        // Sort array by each user's amount. Attendants who owe are at the end of the array.
+        let attendantIndex = 0;
+        while (attendantIndex < actualSpendsForEachAttendant.length && amount < actualSpendsForEachAttendant[attendantIndex].amount) {
+          attendantIndex++;
+        }
+
+        actualSpendsForEachAttendant.splice(attendantIndex, 0, { name: attendant.name, amount })
+      }
+    }
+
+    return actualSpendsForEachAttendant;
+  }
+
+  /**
+   * Calculate users' belongings. Who owes whom and how much.
+   * 
+   * @param actualSpends sorted array of actual spends for each user during the trip
+   * @param totalSpends amount of money spent by this trip in total
+   * @returns 
+   */
+  private calculateStudentsBelongings(actualSpends: { name: string, amount: number }[], totalSpends: number): UserBelongings[] {
+    const eachUserShouldSpend: number = round(totalSpends / actualSpends.length);
+    const userBelongings: UserBelongings[] = [];
+
+    while (actualSpends.length > 1) {
+      const whoOwe = actualSpends[actualSpends.length - 1];
+      const whomOwe = actualSpends[0];
+      const whomExtraSpends = whomOwe.amount - eachUserShouldSpend;
+      let oweAmount = 0;
+
+      if (whomExtraSpends < (eachUserShouldSpend - whoOwe.amount)) {
+        oweAmount = whomExtraSpends;
+        actualSpends.splice(0, 1);
+        actualSpends[actualSpends.length - 1].amount = whoOwe.amount + oweAmount
+      } else {
+        oweAmount = eachUserShouldSpend - whoOwe.amount;
+        actualSpends.splice(actualSpends.length - 1, 1);
+        actualSpends[0].amount = whomOwe.amount - oweAmount;
+      }
+
+      userBelongings.push({
+        who: whoOwe.name,
+        whom: whomOwe.name,
+        amount: round(oweAmount)
+      });
+    }
+
+    return userBelongings
+  }
+
 }
